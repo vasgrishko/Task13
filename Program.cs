91b401ca94184f941be64852de5b627fd91cb440
vas <vas.grishko@yandex.ru>
2022-12-04 03:16:58 +0300

    Дописали программу

--- Program.cs
+++ Program.cs
@@ -6,25 +6,28 @@
 
 // 32679 -> 6
 
-Console.WriteLine("Введите число");
+Console.WriteLine("Введите целое число");
 int num = Convert.ToInt32(Console.ReadLine());
 int result = 0;
-if (num < 100)
+if (num >= 100)
 {    
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    result = num % 10;
+    Console.WriteLine(result);
+    }
+else
+{
     Console.WriteLine("Третьей цифры нет");
 }
-while (num >= 100)
-{
-    if (num > 999)
-    {
-    num = num / 10;
-    result = num % 10;
-    Console.WriteLine(result);
-    break;
-    }  
-    result = num % 10;
-    Console.WriteLine(result);
-    break;
-}
+
+
+    
+   
+   
+    
+
 
     
